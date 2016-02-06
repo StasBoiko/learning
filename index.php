78b1e102f1310da8219effdd569c9272bfb2d0a4
Stasboiko <Stasboiko0@gmail.com>
2016-02-06 10:09:26 +0200

    second coomit+.htaccess

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+
+
+
+<html>
+<head>
+	<title></title>
+	
+</head>
+<body>
+<?php 		
+	$myString="beer"."good";
+	
+	$linktext="ssilka";
+	$linkSrc="http://google.com";
+	
+?>	
+<a href="<?=linkSrc ?>" >
+	<?=$linktext ?>
+	</a>
+</body>
+</html>
